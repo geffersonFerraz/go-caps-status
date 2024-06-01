bee37f3355117d7fc103ff9d7f568dfd700e2fd7
Gefferson Ferraz <me@gefferson.com.br>
2024-06-01 18:08:36 -0300

    general improve

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"bytes"
+	"log"
+	"os"
 	"os/exec"
 	"strings"
+	"syscall"
 	"time"
 
+	"github.com/emersion/go-autostart"
 	"github.com/getlantern/systray"
 )
 
@@ -21,56 +25,99 @@ func Shellout(command string) (string, string, error) {
 	return stdout.String(), stderr.String(), err
 }
 
-func isCapsLockOn() bool {
-
+func isCapsLockOn() int16 {
 	out, _, _ := Shellout("xset q | grep Caps")
 	out = strings.TrimSpace(out)
 	out = strings.TrimPrefix(out, "00: Caps Lock:")
 	out = strings.TrimSpace(out)
 	outArr := strings.Split(out, " ")
 	out = outArr[0]
-
-	return out == "on"
-}
-
-func onReady() {
-	systray.SetTitle("Caps lock")
-
-	for {
-		select {
-		case icon := <-ci:
-			systray.SetIcon(icon)
-		case text := <-ct:
-			systray.SetTitle(text)
-		}
+	if out == "on" {
+		return 1
 	}
-
-}
-
-func onExit() {
+	return 0
 }
 
 var ci chan []byte
 var ct chan string
 
+var app *autostart.App
+var selfLocation string
+
 func main() {
+	selfLocation, _ = os.Executable()
+	// prepare
+	app = &autostart.App{
+		Name:        "GoCAPs",
+		DisplayName: "GoCAPs help you to see when CAPS is On/Off",
+		Exec:        []string{"sh", "-c", selfLocation},
+	}
+
 	ci = make(chan []byte)
 	ct = make(chan string)
-	go systray.Run(onReady, onExit)
 
-	lastState := false
-	firstRun := true
+	go systray.Run(func() {
+		systray.SetTitle("Caps ")
+
+		if app.IsEnabled() {
+			mDisableOrig := systray.AddMenuItem("Disable autostart", "Disable GoCAPS autostart")
+			go func() {
+				<-mDisableOrig.ClickedCh
+				if err := app.Disable(); err != nil {
+					log.Fatal(err)
+				}
+				go RestartSelf()
+				systray.Quit()
+			}()
+		} else {
+			mEnableOrig := systray.AddMenuItem("Enable autostart", "Enable Autostart GoCAPs")
+			go func() {
+				<-mEnableOrig.ClickedCh
+				if err := app.Enable(); err != nil {
+					log.Fatal(err)
+				}
+				go RestartSelf()
+				systray.Quit()
+			}()
+		}
+		systray.AddSeparator()
+
+		mRestartOrig := systray.AddMenuItem("Restart", "Restart the whole app")
+		go func() {
+			<-mRestartOrig.ClickedCh
+			go RestartSelf()
+			systray.Quit()
+		}()
+
+		mQuitOrig := systray.AddMenuItem("Quit", "Quit the whole app")
+		go func() {
+			<-mQuitOrig.ClickedCh
+			systray.Quit()
+		}()
+
+		for {
+			select {
+			case icon := <-ci:
+				systray.SetIcon(icon)
+			case text := <-ct:
+				systray.SetTitle(text)
+			}
+		}
+	}, func() {
+		os.Exit(0)
+	})
+
+	lastState := int16(-1)
 
 	for {
-		time.Sleep(250 * time.Millisecond)
+		time.Sleep(100 * time.Millisecond)
 		stateNow := isCapsLockOn()
-		firstRun = false
-		if !firstRun && stateNow == lastState {
+
+		if stateNow == lastState {
 			continue
 		}
 
-		if stateNow {
-
+		if stateNow == 1 {
 			ci <- conn
 			ct <- "Caps ON"
 
@@ -82,4 +129,10 @@ func main() {
 		lastState = stateNow
 	}
 
+}
+
+func RestartSelf() error {
+	args := os.Args
+	env := os.Environ()
+	return syscall.Exec(selfLocation, args, env)
 }
