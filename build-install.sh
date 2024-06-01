71f95ecb7407a07ae879f38fdab81bdeb2f8fcd6
Gefferson Ferraz <me@gefferson.com.br>
2024-06-01 18:48:10 -0300

    chore: bump to v0.0.7

--- build-install.sh
+++ build-install.sh
@@ -1,3 +1,3 @@
-go build -tags embed -o gocaps
+CGO_ENABLED=1 go build -tags "embed" -ldflags "-s -w" -o gocaps
 
 #now go to: https://askubuntu.com/questions/48321/how-do-i-start-applications-automatically-on-login
